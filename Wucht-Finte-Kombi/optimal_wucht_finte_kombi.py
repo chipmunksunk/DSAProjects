a9f23c288cdc28e31cbe1cf763bca0f5184e1d8b
Daniel Jäger <daniel-jaeger2@arcor.de>
2023-11-15 00:35:24 +0100

    run erstellt, der für gegebene AT/PA/Schaden-Range Datei mit Einträgen erstellt (kann mit np.load() wieder aufgerufen werden)

--- Wucht-Finte-Kombi/optimal_wucht_finte_kombi.py
+++ Wucht-Finte-Kombi/optimal_wucht_finte_kombi.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def Wuchtschlag_Finte_Optimizer(AT_eigen, PA_gegner, Schaden_eigen):
     attack_value = AT_eigen
     parade_value = PA_gegner
@@ -62,7 +63,6 @@ def Wuchtschlag_Finte_Optimizer(AT_eigen, PA_gegner, Schaden_eigen):
 
     return dmg_dealt_list, optimal_wucht_finte
 
-
 def ax_plot(x, y_list, ax, x_label, y_label, title, label_list):
     for y, label in zip(y_list, label_list):
         ax.plot(x, y, label=str(label))
@@ -70,15 +70,15 @@ def ax_plot(x, y_list, ax, x_label, y_label, title, label_list):
     ax.set_ylabel(y_label)
     ax.set_title(title)
 
-def run_1():
 
+def run_1():
     fig, ax = plt.subplots(1, 4)
 
     Optimum_Wucht = []
     Optimum_Finte = []
 
     AT_Liste = range(1, 40)
-    PA_Liste = [5,10,15,20]
+    PA_Liste = [5, 10, 15, 20]
     Schaden_Liste = [5, 10, 15, 20]
 
     for i in range(len(PA_Liste)):
@@ -90,21 +90,23 @@ def run_1():
                 Schaden_list, optimale_Ansage = Wuchtschlag_Finte_Optimizer(AT, PA, Schaden)
                 Finte_Liste.append(optimale_Ansage[0])
                 Wucht_Liste.append(optimale_Ansage[1])
-            ax[i].plot(Wucht_Liste, Finte_Liste, label=str(Schaden), marker='s',markersize=Schaden/5, zorder=20-Schaden)
+            ax[i].plot(Wucht_Liste, Finte_Liste, label=str(Schaden), marker='s', markersize=Schaden / 5,
+                       zorder=20 - Schaden)
         ax[i].set_xlabel('Wucht')
         ax[i].set_xlim((-1, 20))
         ax[i].set_ylim((-1, 20))
-        ax[i].set_title('PA:'+str(PA))
+        ax[i].set_title('PA:' + str(PA))
 
     ax[0].set_ylabel('Finte')
     plt.legend()
     plt.show(block=False)
 
-def run_2(): # compare with formula from https://de.wiki-aventurica.de/wiki/Optimale_Finte-Wuchtschlag-Kombination
+
+def run_2():  # compare with formula from https://de.wiki-aventurica.de/wiki/Optimale_Finte-Wuchtschlag-Kombination
     fig2, ax2 = plt.subplots(1, 4)
 
-    a = range(1,40) # AT
-    b = [5,10,15,20] # PA
+    a = range(1, 40)  # AT
+    b = [5, 10, 15, 20]  # PA
     dmg_base = [5, 10, 15, 20]
     RS = 0
     Z = dmg_base
@@ -115,13 +117,13 @@ def run_2(): # compare with formula from https://de.wiki-aventurica.de/wiki/Opti
             Wucht_Liste = []
             Finte_Liste = []
             for AT in a:
-                #Schaden_list, optimale_Ansage = Wuchtschlag_Finte_Optimizer(AT, PA, Schaden)
-                f_opt = (AT + 2*PA + Schaden - 40) / 3
-                w_opt = (AT - PA - 2*Schaden + 20) / 3
+                # Schaden_list, optimale_Ansage = Wuchtschlag_Finte_Optimizer(AT, PA, Schaden)
+                f_opt = (AT + 2 * PA + Schaden - 40) / 3
+                w_opt = (AT - PA - 2 * Schaden + 20) / 3
                 Finte_Liste.append(f_opt)
                 Wucht_Liste.append(w_opt)
             ax2[i].plot(Wucht_Liste, Finte_Liste, label=str(Schaden), marker='s', markersize=Schaden / 5,
-                       zorder=20 - Schaden)
+                        zorder=20 - Schaden)
             ax2[i].set_xlabel('Wucht')
             ax2[i].set_xlim((-1, 20))
             ax2[i].set_ylim((-1, 20))
@@ -130,3 +132,18 @@ def run_2(): # compare with formula from https://de.wiki-aventurica.de/wiki/Opti
     ax2[0].set_ylabel('Finte')
     plt.legend()
     plt.show()
+
+
+def run_3():  # erstellt ein Array mit Einträgen für die optimalen Wucht-Finte-Ansagen (Achtung: Hohe Laufzeit)
+    AT_range = range(1, 50)
+    PA_range = range(1, 50)
+    Schaden_range = range(1, 20)
+    opt_wucht_finte_array = np.zeros((len(AT_range), len(PA_range), len(Schaden_range), 2))
+    for ind_AT, AT in enumerate(AT_range):
+        for ind_PA, PA in enumerate(PA_range):
+            for ind_Schaden, Schaden in enumerate(Schaden_range):
+                trash, opt_wucht_finte = Wuchtschlag_Finte_Optimizer(AT, PA, Schaden)
+                opt_wucht_finte_array[ind_AT, ind_PA, ind_Schaden, 0] = opt_wucht_finte[0]
+                opt_wucht_finte_array[ind_AT, ind_PA, ind_Schaden, 1] = opt_wucht_finte[1]
+
+    np.save("opt_wucht_finte_file", opt_wucht_finte_array)
